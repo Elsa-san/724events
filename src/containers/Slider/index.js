045332f0b3b3e3fee7dd7ab9e8af965922ff334b
Elsa-san <elsa.tartary@gmail.com>
2023-09-21 10:24:20 +0200

    slider improvement

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,24 +7,18 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus?.sort((evtA, evtB) =>
-  // change the sort (more recent > less recent, the sign was in the wrong way)
-    new Date(evtA.date) > new Date(evtB.date) ? 1 : -1
-  );
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+     (new Date(evtA.date) - new Date(evtB.date))
+  ) ;
+
+  
   const nextCard = () => {
-   const timer = setTimeout(() => {
-      if (byDateDesc && byDateDesc.length > 0) {
-        setIndex(index < byDateDesc.length - 1 ? index + 1 : 0);
-      } else {
-        // Handle the case where byDateDesc is undefined or empty
-        // back on the first element 
-        setIndex(0);
-      }
-    }, 5000);
-return () => {
-  clearTimeout(timer) // Cancel the blank
-}
-}
+setTimeout(
+      () => setIndex(index + 1 < byDateDesc?.length  ? index + 1 : 0),
+      5000
+    )
+  };
+
     
   useEffect(() => {
     nextCard();
@@ -49,13 +43,13 @@ return () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((focus, radioIdx) => (
+              {byDateDesc.map((e, radioIdx) => (
                 <input
-                  key={`${focus.title}`} // key modification
+                  key={`${e.title}`} // key modification
                   type="radio"
                   name={`radio-button ${radioIdx}`}
                   checked={index === radioIdx} // change idx for index for the radio buttons
-                  onChange={() => nextCard(radioIdx)}
+                  onChange={() => this}
                 />
               ))}
             </div>
