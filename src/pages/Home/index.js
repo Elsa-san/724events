5b75994dcba85376b663e171624973d7ceaaa93d
Elsa-san <elsa.tartary@gmail.com>
2023-09-25 09:51:28 +0200

    fix console error on label

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -122,7 +122,7 @@ const Page = () => {
             title={LastData?.title !== undefined ? LastData.title : ""}
             date={new Date(LastData?.date)}
             small
-            label={LastData?.type}
+            label={LastData?.type || ""}
         />
       </div>
       <div className="col contact">
